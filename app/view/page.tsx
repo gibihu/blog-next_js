fa9de1c16d068bb85c04971a7246b775b513077a
gibihu <nawapol0010@gmail.com>
2025-06-09 10:32:46 +0700

    Update page.tsx

--- app/view/page.tsx
+++ app/view/page.tsx
@@ -1,17 +1,17 @@
-
 import PostContent from '../components/posts/onec'; // Adjust the path as necessary
 
 interface Props {
-  searchParams: { id?: string };
+  searchParams: Promise<{ id?: string }>; // Update to match the expected type
 }
 
 export default async function UserPage({ searchParams }: Props) {
-  const id = searchParams.id;
+  const resolvedSearchParams = await searchParams; // Await the Promise
+  const id = resolvedSearchParams.id;
 
   return (
     <>
       <div className="flex flex-col items-center gap-2 p-4 lg:p-6">
-        <div className="p-4 dark:bg-zinc-900 border-2 border-zinc-300 dark:border-zinc-700/50 shadow-lg rounded-lg  max-w-[860px]">
+        <div className="p-4 dark:bg-zinc-900 border-2 border-zinc-300 dark:border-zinc-700/50 shadow-lg rounded-lg max-w-[860px]">
           <PostContent id={Array.isArray(id) ? id[0] : id || ''} />
         </div>
       </div>
